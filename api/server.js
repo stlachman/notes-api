96654ee3331949093c847c8b1f2b06f57e4f205c
Seth Lachman <lachmanseth17@gmail.com>
2019-09-16 14:09:55 -0400

    Added routes folder for todos.

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const server = express();
 
+const todosRouter = require("../todos/todos-router");
+
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
+
+server.use("/todos", todosRouter);
 
 module.exports = server;
