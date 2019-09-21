6fb9065c27a7c735de88a842a99e9d6c02c6c079
Seth Lachman <lachmanseth17@gmail.com>
2019-09-21 16:47:29 -0400

    Added cors package to integrate with client.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const CORS = require("cors");
 const server = express();
 
 const todosRouter = require("../todos/todos-router");
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
-server.use(express.json());
+server.use(bodyParser.json());
+server.use(CORS());
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
